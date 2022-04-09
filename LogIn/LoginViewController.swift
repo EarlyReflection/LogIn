445df84ca3b5a03d4b82df45650af0b73e2e5214
Vladimir Dvornikov <echobizarre@gmail.com>
2022-04-09 16:15:57 +0300

    Added alert controllers

--- LogIn/LoginViewController.swift
+++ LogIn/LoginViewController.swift
@@ -12,25 +12,52 @@ class ViewController: UIViewController {
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    private let username = "qwerty"
+    private let password = "12345"
+    
+    // prepare - передает данные из LoginViewController в LogOutViewController
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let logOutVC = segue.destination as? LogOutViewController else {return}
+        logOutVC.greeting = "Hello, \(username)!"
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let logOutVC = segue.destination as? LogOutViewController else {return} // скастил logOutVC до LogOutViewController, извлек опционал с помощью guard let
-        logOutVC.greeting = "Hello, \(userNameTextField.text ?? "")!"
+    @IBAction func logInPressed() {
+        if  userNameTextField.text != username || passwordTextField.text != password {
+            showAlert(
+                with: "Invalid login or password",
+                and: "Enter correct login and password"
+            )
+            passwordTextField.text = ""
+        }
     }
     
     @IBAction func forgotUserNamePressed() {
+        showAlert(with: "Ooops!", and: " Your name is: \(username)")
+        userNameTextField.text = ""
+        
     }
     
     @IBAction func forgotPasswordPressed() {
+        showAlert(with: "Ooops!", and: " Your password is: \(password)")
+        passwordTextField.text = ""
     }
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
         userNameTextField.text = ""
+        passwordTextField.text = ""
     }
 }
+
+
+// MARK: - Alert Controller
+extension ViewController {
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
 
 
